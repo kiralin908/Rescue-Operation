a3198320cfee28c7295457824c2a193ce8051204
kiralin <highfoot908@livemail.tw>
2024-01-07 21:13:42 +0800

    Initial Commit

---
+++ Rescue Operation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rescue Operation
+//
+//  Created by 林郁琦 on 2024/1/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
